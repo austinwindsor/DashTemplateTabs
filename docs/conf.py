2fdba2cd0663a13c23971a986f8f643516632ae8
Austin Windsor <windsor929@gmail.com>
2021-09-22 00:18:54 -0400

    add glossary"

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc',
-'sphinx.ext.doctest']
+'sphinx.ext.doctest','sphinx.ext.glossary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
