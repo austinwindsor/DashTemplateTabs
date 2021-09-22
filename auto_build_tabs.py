4548fdb1e75f538d9f70e1656b4a08a8b8e55143
Austin Windsor <windsor929@gmail.com>
2021-09-22 00:18:29 -0400

    update formatting

--- auto_build_tabs.py
+++ auto_build_tabs.py
@@ -1,8 +1,11 @@
+"""Summary
+"""
 from importlib import import_module
 import os
 from glob import glob
 
 import sys
+import re
 
 from dash import html
 from dash import dcc
@@ -12,6 +15,11 @@ from dash.dependencies import Input, Output, State
 from collections import OrderedDict
 
 from app import app
+import docstring_parser
+
+import pandas as pd
+
+import logging
 
 #### PARAMETERS #####
 # FUNCTIONS_DIR = 'funcs'
@@ -41,34 +49,50 @@ def get_argparse_parameters(script, argparser_parser='parser', file_handle='.py'
 	func_param_dtypes = OrderedDict([(k,v) for k,v in func_parsers._option_string_actions.items()
 						if k not in argparse_help_commands])
 	# print( {func: func_param_dtypes} )
-	return {func: {"parameters": func_param_dtypes, "description": func_parsers.prog}}
+	return {func: {"parameters": func_param_dtypes, "description": func_parsers.prog}}\
+
+def get_method_parameters(module, run_method='run', display_name=""):
+    """Summary
+    
+    Args:
+        method_name (TYPE): Description
+    """
+    display_name = display_name if display_name else module.__name__.split('.')[0]
+    method_parsing = docstring_parser.parse(getattr(module,run_method).__doc__)
+    parameters = method_parsing.params
+    description = method_parsing.short_description +'\n'+method_parsing.long_description
+    return {display_name: {'parameters':[param for param in parameters], "description": description}}
 
 
-def param_to_dash_elem(argparse_store_action, param_name, function_name):
+def param_to_dash_elem(argparse_store_action, function_name):
 	"""
 	This method converts an Argparse.parser._StoreAction object into an appropriate input field. 
 	A integer,string parameter will use a empty field.
 	A parameter with specific choices to choose from will use a dcc.dropdown field.
-	A parameter with many entries is still unclear how to handle.
+	A parameter with many entries is still unclear how to handle.	
 	All parameters with default values will have that value prepopulated in the input field.
 	"""
-	param_name = param_name.replace('-','')
-	dtype = argparse_store_action.type
+	param_name = argparse_store_action.arg_name
+	dtype = str(argparse_store_action.type_name)
 	default = argparse_store_action.default
-	help_str = argparse_store_action.help
+	help_str = argparse_store_action.description
+	optional_status = argparse_store_action.is_optional
+	choices = set(re.findall("\{(.+)\}", help_str)[0].split(',')) if len(re.findall("\{(.+)\}", help_str))>0 else None
+	print(help_str, choices)
 
 	id_name = 'parameter_%s_%s' % (function_name, param_name)
-	type_map = {int: 'number', str: 'text', float: 'number'}
+	type_map = {'int': 'number', 'str': 'text', 'float': 'number'}
 
-	if argparse_store_action.choices is None:
-		output_dash_elem = dbc.Row([ dbc.Col(html.Label(pretty_format(param_name), title=help_str)), dbc.Col(dcc.Input(id=id_name, 
-																		type=type_map.get(dtype,'text'), 
-																		placeholder=str(default)))])
+	if choices is None:
+		output_dash_elem = dbc.Row([ dbc.Col(html.Label(pretty_format(param_name), title=help_str)), 
+											dbc.Col(dcc.Input(id=id_name, 
+															type=type_map.get(dtype,'text'), 
+															placeholder=str(default)))])
 	else:
 		output_dash_elem = dbc.Row([ dbc.Col(html.Label(pretty_format(param_name), title=help_str)), 
 									dbc.Col(dcc.Dropdown(id=id_name, options=[{'label':c, 'value':c} 
-																		for c in argparse_store_action.choices]
-												, value=default))])
+																				for c in choices]
+														, value=default))])
 	# print(output_dash_elem)
 	return output_dash_elem
 
@@ -84,13 +108,13 @@ def dash_tab_for_cmd_args(function_parameters):
 	description = list(function_parameters.values())[0]['description']
 	logging_screen = dbc.Card([
 						dbc.CardHeader("Logging Screen"),
-						dbc.CardBody(html.P(id={"type":"logging", "name":function_name+"-"+"log1"}), id=function_name+"-"+"log")
+						dbc.CardBody(dcc.Markdown(id=function_name+"-"+"log1"), id=function_name+"-"+"log")
 		])
 	param_screen = dbc.Card(children=[
 						dbc.CardHeader("Input Parameters"),
 						dbc.CardBody([
-							param_to_dash_elem(store_action, param_name, function_name) for param_name, store_action
-							in parameters.items()
+							param_to_dash_elem(store_action, function_name) for store_action
+							in parameters
 							])
 		])
 	description_screen = dbc.Card(children=[
@@ -102,24 +126,34 @@ def dash_tab_for_cmd_args(function_parameters):
 					label=func_pretty_name,
 					children = html.Div( [  dbc.Row( dbc.Col(description_screen)),
 											dbc.Row([ dbc.Col(param_screen), dbc.Col(logging_screen)]),
-											dbc.Row( dbc.Col(html.Button("Execute %s" % func_pretty_name, 
-												id=function_name+"-"+"button")) )]))
+											dbc.Row( [dbc.Col(html.Button("Execute %s" % func_pretty_name, 
+																			id=function_name+"-"+"button")),
+													dbc.Col([html.Button("Download Output", 
+																			id=function_name+"-"+"downloadButton"),
+															dcc.Download(id=function_name+"-"+"download")]) ]),
+											dcc.Interval(id=function_name+"-"+'loggingInterval', interval=1*1000, n_intervals=0)
+											]))
 
-def build_tab_per_function(module_paths, argparse_parser='parser', argparse_help_commands=['--help','-h']):
+def build_tab_per_function(module_objs, run_method='run', ignore_parameters=['--help','-h']):
 	"""
 	function to create a list of dcc.Tabs where each tab contains input elements for each function parameter
-
-	:param module_paths: a list of the paths to each function to visualize
-	:param argparse_parser: the name of the variable of the Argparse.parser object in the scripts
-	:param argparse_help_commands: the helper command line arguments to exclude when visualizing the command line arguments
+	
+	Args:
+	    module_objs (list of [modules]): modules or submodules for which to create a Dash Plotly interface
+	    run_method (str): name of the method that contains the run parameters for task
+	    ignore_parameters (list of [str], optional): list of parameters to not include in visualization
+	
+	Returns:
+	    list of [dash_core_component.Tab]: a list of the tabs to visualize
 	"""
-	func_parameters = list(map(lambda func: get_argparse_parameters(func, argparser_parser=argparse_parser),
-						[f.replace("/",'.').replace('\\','.') for f in module_paths]))
+	func_parameters = list(map(lambda func: get_method_parameters(func['module'], run_method=run_method, 
+																	display_name=func['display_name']),
+							[f for f in module_objs]))
 	tabs = list(map(dash_tab_for_cmd_args, func_parameters))
 	return tabs
 
 
-def build_dynamic_callbacks(module_paths, argparse_parser='parser', argparse_help_commands=['--help','-h']):
+def build_dynamic_callbacks(module_objs, run_method='run', argparse_help_commands=['--help','-h']):
 	"""
 	function to create many dynamic callback functions linked to each element explicitly
 
@@ -127,42 +161,51 @@ def build_dynamic_callbacks(module_paths, argparse_parser='parser', argparse_hel
 	:param argparse_parser: the name of the variable of the Argparse.parser object in the scripts
 	:param argparse_help_commands: the helper command line arguments to exclude when visualizing the command line arguments
 	"""
-	func_parameters = list(map(lambda func: get_argparse_parameters(func, argparser_parser=argparse_parser),
-						[f.replace("/",'.').replace('\\','.') for f in module_paths]))
-	# print(func_parameters)
-	for func in func_parameters:
+	func_parameters = list(map(lambda func: get_method_parameters(func['module'], run_method=run_method, 
+																	display_name=func['display_name']),
+							[f for f in module_objs]))
+	modules = [f['module'] for f in module_objs]
+
+	for mod, func in zip( modules, func_parameters):
 		parameters = list(func.values())[0]['parameters']#list(list(func.values())[0].keys())
 
 		func_path = list(func.keys())[0].replace('.','/') +".py"
-		clean_func_name = list(func.keys())[0].replace('-','').split('.')[-1]
+		clean_func_name = list(func.keys())[0]
 
 		@app.callback(
-			Output(clean_func_name+"-log", "children"),
+			Output(clean_func_name+"-download", "data"),
 			Input("%s-button" % clean_func_name, "n_clicks"),
-			[State("parameter_%s_%s" % (clean_func_name, inp.replace("-","")),'value') for inp in parameters]
+			[State("parameter_%s_%s" % (clean_func_name, inp.arg_name),'value') for inp in parameters]
 			)
 		def func(nclicks, *args, parameters=parameters, func_path=func_path):
 			arguments = args
 			nclicks = nclicks
 			if nclicks:
-				parameter_str = ' '.join(['%s "%s"' % (str(param), str(value)) for param, value in zip(parameters, arguments)])
+				parameter_str = ' '.join(['%s "%s"' % (str(param.arg_name), str(value)) for param, value in zip(parameters, arguments)])
+				data = getattr(mod, run_method)(**{param.arg_name:a for param, a in zip(parameters, args)})
 				command = 'python %s %s' % (func_path, parameter_str)
-				print(command)
-				os.system(command)
-				return command
+				logging.info(command)
+				# os.system(command)
+				# print(globals()['log_capture_string'].get_value())
+				return  dcc.send_data_frame(data.to_excel, 'sample.xlsx')
+
+		# @app.callback(
+		# 	Output(clean_func_name+"-download","data"),
+		# 	Input(clean_func_name+"-downloadButton", "n_clicks"),
+		# 	prevent_initial_callback=True
+		# 	)
+		# def download(n_clicks):
+		# 	df = pd.DataFrame({'a':[1,2]})
+		# 	return dcc.send_data_frame(df.to_excel, 'sample.xlsx')
+
+		@app.callback(
+			Output(clean_func_name+'-log1', 'children'),
+			Input(clean_func_name+'-loggingInterval', 'n_intervals')
+			)
+		def log_screen(*args):
+			with open('app.log', 'r') as f:
+				return f.read()
 		
 		globals()['execute_%s' % clean_func_name] = func
+		globals()['log_%s' % clean_func_name] = log_screen
 
-
-# @app.callback(
-# 	Output({"type":"logging"}, "children"),
-# 	[Input({"type":"parameter"}, "value"),
-# 	 Input("Consolidate_files", "nclicks"),
-# 	 Input("Consolidate_files", "label")]
-# 	)
-# def execute_function(values, nclicks, button_name):
-# 	command = "python %s %s" % (button_name, ' '.join(values))
-# 	return command
-
-
-# print(dash_tab_for_cmd_args(list(output)[0]))
